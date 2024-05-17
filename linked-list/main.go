1a84f9950ec13e15fa92b7b075739c432d67dd7b
Frank <franklaercio@gmail.com>
2024-05-17 00:57:25 -0300

    feat: adding display a linked list and adjusting removing method

--- linked-list/main.go
+++ linked-list/main.go
@@ -15,7 +15,7 @@ type LinkedList struct {
 
 // Add is a function that insert a value in O(1), because inserting a some value in linked list is easy
 func (list *LinkedList) Add() {
-	fmt.Print("Insert your value -> ")
+	fmt.Print("Insert your value: ")
 
 	var value int
 	if _, err := fmt.Scanf("%d", &value); err != nil {
@@ -27,59 +27,67 @@ func (list *LinkedList) Add() {
 	if list.head == nil {
 		list.head = newNode
 		list.tail = newNode
+	} else {
+		list.tail.next = newNode
+		list.tail = newNode
 	}
 
-	list.tail.next = newNode
-	list.tail = newNode
 	list.size++
 }
 
+// Remove is a function that remove a value in O(1), because removing a some value in linked list is easy in head
 func (list *LinkedList) Remove() {
-	fmt.Print("Insert your value that should be removed -> ")
-
-	var value int
-	if _, err := fmt.Scanf("%d", &value); err != nil {
-		fmt.Println("Invalid input, try again")
-		list.Add()
-	}
-
 	if list.head == nil {
 		fmt.Println("list is empty")
+		return
 	}
 
-	for {
-		// TODO: change to removing just a tail value to removing in O(1)
-		if list.head.value == value {
-			list.head = list.head.next
-			list.size--
+	fmt.Println("Removing:", list.head.value)
 
-			if list.size == 0 {
-				list.tail = nil
-			}
-
-			break
-		}
-
-		if list.head.next == nil {
-			break
-		}
-
+	if list.head == list.tail {
+		list.head = nil
+		list.tail = nil
+	} else {
 		list.head = list.head.next
+		list.tail.next = nil
 	}
+	list.size--
+}
+
+// Display is a function that display all values in linked list in O(n)
+func (list *LinkedList) Display() {
+	if list.head == nil {
+		fmt.Println("List is empty")
+		return
+	}
+
+	fmt.Printf("There are %d in list: ", list.size)
+	current := list.head
+	for current != nil {
+		fmt.Print(current.value)
+		if current.next != nil {
+			fmt.Print(" -> ")
+		}
+		current = current.next
+	}
+	fmt.Println()
 }
 
 func main() {
 	list := LinkedList{}
 
 	for {
-		fmt.Println("1 - Insert a value")
+		fmt.Println("\n1 - Insert a value")
 		fmt.Println("2 - Remove a value")
 		fmt.Println("3 - Display list")
+		fmt.Println("4 - Exit")
 
 		var option int
+		fmt.Print("\nInsert your option: ")
+
 		if _, err := fmt.Scanf("%d", &option); err != nil {
-			fmt.Println("Invalid option")
-			continue
+			fmt.Println("\nInvalid option")
+			panic(err)
 		}
 
 		switch option {
@@ -87,6 +95,10 @@ func main() {
 			list.Add()
 		case 2:
 			list.Remove()
+		case 3:
+			list.Display()
+		case 4:
+			return
 		}
 	}
 }
